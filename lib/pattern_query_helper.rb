c6fe357eacaf7ab85dfe59052d74e1d0d192df8a
Evan McDaniel <eamigo13@gmail.com>
2019-02-28 12:02:00 -0700

    run_sql_query and run_active_record_query methods.  v0.1.5

--- lib/pattern_query_helper.rb
+++ lib/pattern_query_helper.rb
@@ -6,8 +6,32 @@ require "pattern_query_helper/sorting"
 require "pattern_query_helper/sql"
 
 module PatternQueryHelper
-  def self.paginated_sql_query(model, query, query_params, url_params)
-    query_helpers = parse_params(url_params)
+  def self.run_sql_query(model, query, query_params, query_helpers, query_type)
+    case query_type
+    when "single_record"
+      single_record_sql_query(model, query, query_params, query_helpers)
+    when "paginated"
+      paginated_sql_query(model, query, query_params, query_helpers)
+    when "all"
+      sql_query(model, query, query_params, query_helpers)
+    end
+  end
+
+  def self.run_active_record_query(active_record_call, query_helpers, query_type)
+    case query_type
+    when "single_record"
+      single_record_active_record_query(active_record_call, query_helpers)
+    when "paginated"
+      paginated_active_record_query(active_record_call, query_helpers)
+    when "all"
+      active_record_query(active_record_call, query_helpers)
+    end
+  end
+
+  private
+
+  def self.paginated_sql_query(model, query, query_params, query_helpers)
+    query_helpers = parse_helpers(query_helpers)
 
     query_config = {
       model: model,
@@ -31,8 +55,8 @@ module PatternQueryHelper
     }
   end
 
-  def self.sql_query(model, query, query_params, url_params)
-    query_helpers = parse_params(url_params)
+  def self.sql_query(model, query, query_params, query_helpers)
+    query_helpers = parse_helpers(query_helpers)
 
     query_config = {
       model: model,
@@ -51,8 +75,8 @@ module PatternQueryHelper
     }
   end
 
-  def self.single_record_sql_query(model, query, query_params, url_params)
-    query_helpers = parse_params(url_params)
+  def self.single_record_sql_query(model, query, query_params, query_helpers)
+    query_helpers = parse_helpers(query_helpers)
 
     query_config = {
       model: model,
@@ -71,8 +95,8 @@ module PatternQueryHelper
     }
   end
 
-  def self.active_record_query(active_record_call, url_params)
-    query_helpers = parse_params(url_params)
+  def self.active_record_query(active_record_call, query_helpers)
+    query_helpers = parse_helpers(query_helpers)
     filtered_query = PatternQueryHelper::Filtering.filter_active_record_query(active_record_call, query_helpers[:filters])
     sorted_query = PatternQueryHelper::Sorting.sort_active_record_query(active_record_call, query_helpers[:sorting])
     with_associations = PatternQueryHelper::Associations.load_associations(sorted_query, query_helpers[:associations])
@@ -81,8 +105,8 @@ module PatternQueryHelper
     }
   end
 
-  def self.paginated_active_record_query(active_record_call, url_params)
-    query_helpers = parse_params(url_params)
+  def self.paginated_active_record_query(active_record_call, query_helpers)
+    query_helpers = parse_helpers(query_helpers)
     filtered_query = PatternQueryHelper::Filtering.filter_active_record_query(active_record_call, query_helpers[:filters])
     sorted_query = PatternQueryHelper::Sorting.sort_active_record_query(active_record_call, query_helpers[:sorting])
     paginated_query = PatternQueryHelper::Pagination.paginate_active_record_query(sorted_query, query_helpers[:pagination])
@@ -94,11 +118,11 @@ module PatternQueryHelper
     }
   end
 
-  def self.single_record_active_record_query
+  def self.single_record_active_record_query(active_record_call, query_helpers)
     # TODO: Add Logic to this method
   end
 
-  def self.parse_params(params)
+  def self.parse_helpers(params)
     filtering = PatternQueryHelper::Filtering.create_filters(params)
     sorting = PatternQueryHelper::Sorting.parse_sorting_params(params)
     associations = PatternQueryHelper::Associations.process_association_params(params)
--- spec/pattern_query_helper_spec.rb
+++ spec/pattern_query_helper_spec.rb
@@ -1,28 +1,28 @@
 RSpec.describe PatternQueryHelper do
   it "returns a paginated sql query" do
-    results = PatternQueryHelper.paginated_sql_query(Child, "select * from children", {}, @url_params)
+    results = PatternQueryHelper.run_sql_query(Child, "select * from children", {}, @url_params, "paginated")
     expect(results[:pagination]).to_not be nil
     expect(results[:data]).to_not be nil
   end
 
   it "returns a sql query" do
-    results = PatternQueryHelper.sql_query(Child, "select * from children", {}, @url_params)
+    results = PatternQueryHelper.run_sql_query(Child, "select * from children", {}, @url_params, "all")
     expect(results[:data]).to_not be nil
   end
 
   it "returns a single record sql query" do
     @url_params[:filter] = nil
-    results = PatternQueryHelper.single_record_sql_query(Child, "select * from children where id=#{Child.first.id}", {}, @url_params)
+    results = PatternQueryHelper.run_sql_query(Child, "select * from children where id=#{Child.first.id}", {}, @url_params, "single_record")
     expect(results[:data]).to_not be nil
   end
 
   it "returns an active record query" do
-    results = PatternQueryHelper.active_record_query(Child.all, @url_params)
+    results = PatternQueryHelper.run_active_record_query(Child.all, @url_params, "all")
     expect(results[:data]).to_not be nil
   end
 
   it "returns a paginated active record query" do
-    results = PatternQueryHelper.paginated_active_record_query(Child.all, @url_params)
+    results = PatternQueryHelper.run_active_record_query(Child.all, @url_params, "paginated")
     expect(results[:pagination]).to_not be nil
     expect(results[:data]).to_not be nil
   end
