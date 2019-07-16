32f91cbb2880a9bae20cce3735de22f73e669e79
Evan McDaniel <eamigo13@gmail.com>
2019-07-16 11:07:41 -0600

    uncomment some tests

--- spec/fixtures/example_queries.rb
+++ spec/fixtures/example_queries.rb
@@ -1,129 +1,129 @@
 module ExampleQueries
   SQL_QUERIES = [
-    # {
-    #   query: %(
-    #     select c.id, c.name, c.age
-    #     from children c
-    #   ),
-    #   model: Child,
-    #   expected_sorts: ["id", "name", "age"],
-    #   expected_filters: [
-    #     {
-    #       alias: "id",
-    #       operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
-    #       class: Integer
-    #     },
-    #     {
-    #       alias: "age",
-    #       operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
-    #       class: Integer
-    #     },
-    #     {
-    #       alias: "name",
-    #       operator_codes: ["like"],
-    #       class: String
-    #     },
-    #     {
-    #       alias: "name",
-    #       operator_codes: ["in, not_in"],
-    #       class: Array
-    #     },
-    #     {
-    #       alias: "age",
-    #       operator_codes: ["null"],
-    #       class: TrueClass
-    #     },
-    #   ]
-    # },
-    # {
-    #   query: %(
-    #     select p.name, p.age, count(c.id) as children_count
-    #     from parents p join children c on c.parent_id = p.id
-    #     where p.age > 35
-    #     group by p.name
-    #     having count(c.id) > 3
-    #     order by p.name
-    #     limit 1
-    #   ),
-    #   model: Child,
-    #   expected_sorts: ["name", "age", "children_count"],
-    #   expected_filters: [
-    #     {
-    #       alias: "id",
-    #       operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
-    #       class: Integer
-    #     },
-    #     {
-    #       alias: "age",
-    #       operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
-    #       class: Integer
-    #     },
-    #     {
-    #       alias: "name",
-    #       operator_codes: ["like"],
-    #       class: String
-    #     },
-    #     {
-    #       alias: "name",
-    #       operator_codes: ["in, not_in"],
-    #       class: Array
-    #     },
-    #     {
-    #       alias: "age",
-    #       operator_codes: ["null"],
-    #       class: TrueClass
-    #     },
-    #     {
-    #       alias: "children_count",
-    #       operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
-    #       class: Integer
-    #     },
-    #   ]
-    # },
-    # {
-    #   query: %(
-    #     with children_count as (
-    #       select p.id as parent_id, p.age, count(c.id) as children_count
-    #       from parents p join children c on c.parent_id = p.id
-    #       group by p.id
-    #     ),
-    #     max_age as (
-    #       select p.id as parent_id, max(c.age) as max_age
-    #       from parents p join children c on c.parent_id = p.id
-    #       group by p.id
-    #     )
-    #     select p.name, c.children_count, m.max_age
-    #     from parents p
-    #     join children_count c on p.id = c.parent_id
-    #     join max_age m on p.id = m.parent_id
-    #   ),
-    #   model: Child,
-    #   expected_sorts: ["name", "children_count", "max_age"]
-    # },
-    # {
-    #   query: %(
-    #     select
-    #       p.name,
-    #       (
-    #         select max(c2.age)
-    #         from parents p2 join children c2 on c2.parent_id = p2.id
-    #         where p2.id = p.id
-    #         group by p2.id
-    #       ) as max_age
-    #     from parents p
-    #   ),
-    #   model: Child,
-    #   expected_sorts: ["name", "max_age"]
-    # },
-    # {
-    #   query: %(
-    #     select c.id, c.name, c.age, p.name as parent_name
-    #     from children c
-    #     join parents p on p.id = c.parent_id
-    #   ),
-    #   model: Child,
-    #   expected_sorts: ["id", "name", "age", "parent_name"]
-    # },
+    {
+      query: %(
+        select c.id, c.name, c.age
+        from children c
+      ),
+      model: Child,
+      expected_sorts: ["id", "name", "age"],
+      expected_filters: [
+        {
+          alias: "id",
+          operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
+          class: Integer
+        },
+        {
+          alias: "age",
+          operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
+          class: Integer
+        },
+        {
+          alias: "name",
+          operator_codes: ["like"],
+          class: String
+        },
+        {
+          alias: "name",
+          operator_codes: ["in, not_in"],
+          class: Array
+        },
+        {
+          alias: "age",
+          operator_codes: ["null"],
+          class: TrueClass
+        },
+      ]
+    },
+    {
+      query: %(
+        select p.name, p.age, count(c.id) as children_count
+        from parents p join children c on c.parent_id = p.id
+        where p.age > 35
+        group by p.name
+        having count(c.id) > 3
+        order by p.name
+        limit 1
+      ),
+      model: Child,
+      expected_sorts: ["name", "age", "children_count"],
+      expected_filters: [
+        {
+          alias: "id",
+          operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
+          class: Integer
+        },
+        {
+          alias: "age",
+          operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
+          class: Integer
+        },
+        {
+          alias: "name",
+          operator_codes: ["like"],
+          class: String
+        },
+        {
+          alias: "name",
+          operator_codes: ["in, not_in"],
+          class: Array
+        },
+        {
+          alias: "age",
+          operator_codes: ["null"],
+          class: TrueClass
+        },
+        {
+          alias: "children_count",
+          operator_codes: ["gte", "gt", "lte", "lt", "eql", "noteql"],
+          class: Integer
+        },
+      ]
+    },
+    {
+      query: %(
+        with children_count as (
+          select p.id as parent_id, p.age, count(c.id) as children_count
+          from parents p join children c on c.parent_id = p.id
+          group by p.id
+        ),
+        max_age as (
+          select p.id as parent_id, max(c.age) as max_age
+          from parents p join children c on c.parent_id = p.id
+          group by p.id
+        )
+        select p.name, c.children_count, m.max_age
+        from parents p
+        join children_count c on p.id = c.parent_id
+        join max_age m on p.id = m.parent_id
+      ),
+      model: Child,
+      expected_sorts: ["name", "children_count", "max_age"]
+    },
+    {
+      query: %(
+        select
+          p.name,
+          (
+            select max(c2.age)
+            from parents p2 join children c2 on c2.parent_id = p2.id
+            where p2.id = p.id
+            group by p2.id
+          ) as max_age
+        from parents p
+      ),
+      model: Child,
+      expected_sorts: ["name", "max_age"]
+    },
+    {
+      query: %(
+        select c.id, c.name, c.age, p.name as parent_name
+        from children c
+        join parents p on p.id = c.parent_id
+      ),
+      model: Child,
+      expected_sorts: ["id", "name", "age", "parent_name"]
+    },
     {
       query: %(
         select
